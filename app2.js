7a96443fa184c47cc8db3fd2e33ce5800069c758
sararat <31531612+sararat@users.noreply.github.com>
2023-10-31 22:16:00 +0700

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+console.log(response.data);
+}).catch(error => {
+console.error('error');
+});
